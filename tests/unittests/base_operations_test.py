f80b570feaf407c7a3f944c2017ad85ea54fdf5a
Georgij <netbay.georgij@gmail.com>
2021-04-11 04:25:32 +0500

    Восстанавливаем тесты для всех стандартных операций

--- tests/unittests/base_operations_test.py
+++ tests/unittests/base_operations_test.py
@@ -10,3 +10,26 @@ class BaseOperationsTest:
 
         await repository.set_one(setting_name, setting_value)
         assert setting_value == await repository.get_one(setting_name)
+
+    @pytest.mark.asyncio
+    async def test_set_many_get_many(self, repository):
+        settings = {
+            "a": 10,
+            "b": "b"
+        }
+
+        await repository.set_many(settings)
+        settings_names = list(settings.keys())
+        result_settings = await repository.get_many(settings_names)
+        assert settings == result_settings
+
+    @pytest.mark.asyncio
+    async def test_set_all_get_all(self, repository):
+        settings = {
+            "a": 10,
+            "b": "b"
+        }
+
+        await repository.set_all(settings)
+        result_settings = await repository.get_all()
+        assert settings == result_settings
