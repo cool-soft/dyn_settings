58434741175f7ae1740362d961561cc3ceb414da
Georgij <netbay.georgij@gmail.com>
2021-04-13 14:55:06 +0500

    ~~~

--- tests/unittests/base_operations_test.py
+++ tests/unittests/base_operations_test.py
@@ -9,9 +9,12 @@ class BaseRepositoryOperationsTest:
     async def test_set_one_get_one(self, repository):
         setting_name = "setting"
         values = [None, True, False, 10, 50.23, "string value", datetime.now()]
+
         for setting_value in values:
             await repository.set_one(setting_name, setting_value)
-            assert setting_value == await repository.get_one(setting_name)
+
+            result_setting = await repository.get_one(setting_name)
+            assert setting_value == result_setting
 
     @pytest.mark.asyncio
     async def test_set_many_get_many(self, repository):
@@ -26,6 +29,7 @@ class BaseRepositoryOperationsTest:
         }
 
         await repository.set_many(settings)
+
         settings_names = list(settings.keys())
         result_settings = await repository.get_many(settings_names)
         assert settings == result_settings
@@ -49,5 +53,6 @@ class BaseRepositoryOperationsTest:
 
         await repository.set_many(fake_setting)
         await repository.set_all(settings)
+
         result_settings = await repository.get_all()
         assert settings == result_settings
