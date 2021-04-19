1eb8179be86a12c52191f04728d78abb7cace844
Georgij <netbay.georgij@gmail.com>
2021-04-19 19:18:47 +0500

    Переносим создание объектов с динамической настройкой на factory

--- src/dynamic_settings/dynamic_settings_factory.py
+++ src/dynamic_settings/dynamic_settings_factory.py
@@ -1,6 +1,8 @@
 import logging
 from typing import List, Optional, Any, Dict
 
+from aiorwlock import RWLock
+
 from dynamic_settings.repository.settings_repository import SettingsRepository
 
 
@@ -9,6 +11,7 @@ class DSFactory:
     def __init__(self,
                  class_: Optional = None,
                  settings_repository: Optional[SettingsRepository] = None,
+                 settings_rwlock: Optional[RWLock] = None,
                  settings_names: Optional[List[str]] = None,
                  args: Optional[List[Any]] = None,
                  kwargs: Optional[Dict[str, Any]] = None) -> None:
@@ -18,7 +21,8 @@ class DSFactory:
 
         self._class = class_
         self._settings_repository = settings_repository
-        self._dynamic_settings_names = settings_names
+        self._settings_rwlock = settings_rwlock
+        self._settings_names = settings_names
 
         if args is None:
             args = []
@@ -39,7 +43,11 @@ class DSFactory:
 
     def set_settings_names(self, settings_names: List[str]) -> None:
         self._logger.debug(f"Settings names are set to {settings_names}")
-        self._dynamic_settings_names = settings_names
+        self._settings_names = settings_names
+
+    def set_settings_rwlock(self, lock: RWLock) -> None:
+        self._logger.debug(f"Settings rwlock is set to {lock}")
+        self._settings_rwlock = lock
 
     def set_args(self, args: List[Any]) -> None:
         self._logger.debug(f"Args are set to {args}")
@@ -49,13 +57,17 @@ class DSFactory:
         self._logger.debug(f"Keyword args are set to {kwargs}")
         self._kwargs = kwargs
 
+    async def __call__(self):
+        return await self.create_instance()
+
     async def create_instance(self) -> Any:
         self._logger.debug(f"Creating instance of {self._class.__name__}")
 
         args = self._args.copy()
         kwargs = self._kwargs.copy()
 
-        dynamic_settings = await self._settings_repository.get_many(self._dynamic_settings_names)
+        async with self._settings_rwlock.reader_lock:
+            dynamic_settings = await self._settings_repository.get_many(self._settings_names)
         kwargs.update(dynamic_settings)
 
         instance = self._class(*args, **kwargs)
---
+++ tests/unittests/test_dynamic_settings_service.py
@@ -0,0 +1,62 @@
+from copy import copy
+
+import pytest
+from aiorwlock import RWLock
+
+from dynamic_settings.ds_service.async_dynamic_settings_service import AsyncDynamicSettingsService
+from dynamic_settings.repository.simple_settings_repository import SimpleSettingsRepository
+
+
+class TestDynamicSettingsService:
+
+    @pytest.fixture
+    def settings(self):
+        return {
+            "setting_a": 0,
+            "setting_b": 1
+        }
+
+    @pytest.fixture
+    def default_settings(self):
+        return {
+            "setting_a": 10,
+            "setting_c": False
+        }
+
+    @pytest.fixture
+    def settings_repository(self):
+        return SimpleSettingsRepository()
+
+    @pytest.fixture
+    async def settings_rwlock(self):
+        return RWLock()
+
+    # noinspection PyTypeChecker
+    @pytest.fixture
+    async def settings_service(self, default_settings, settings_rwlock, settings_repository):
+        return AsyncDynamicSettingsService(
+            settings_repository=settings_repository,
+            settings_rwlock=settings_rwlock,
+            defaults=default_settings
+        )
+
+    # noinspection PyUnresolvedReferences
+    @pytest.mark.asyncio
+    async def test_dynamic_settings_di_service(self,
+                                               settings_service,
+                                               settings_repository,
+                                               settings,
+                                               default_settings,
+                                               settings_rwlock):
+
+        await settings_service.initialize_repository()
+        await settings_service.set_settings(settings)
+
+        combined_settings = copy(default_settings)
+        combined_settings.update(settings)
+        setting_names = list(combined_settings.keys())
+
+        async with settings_rwlock.reader_lock:
+            result_settings = await settings_repository.get_many(setting_names)
+
+        assert combined_settings == result_settings
