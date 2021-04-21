00f807353dba1a89df43169475769ac00e96609d
Georgij <netbay.georgij@gmail.com>
2021-04-21 22:36:38 +0500

    Внедряем SettingsService в factory

--- src/dynamic_settings/factory/async_factory.py
+++ src/dynamic_settings/factory/async_factory.py
@@ -1,14 +1,14 @@
 import logging
 from typing import List, Optional, Any, Dict
 
-from dynamic_settings.repository.settings_repository import SettingsRepository
+from dynamic_settings.service.async_settings_service import AsyncSettingsService
 
 
-class DSFactory:
+class AsyncFactory:
 
     def __init__(self,
                  class_: Optional = None,
-                 settings_repository: Optional[SettingsRepository] = None,
+                 settings_service: Optional[AsyncSettingsService] = None,
                  settings_names: Optional[List[str]] = None,
                  args: Optional[List[Any]] = None,
                  kwargs: Optional[Dict[str, Any]] = None) -> None:
@@ -17,7 +17,7 @@ class DSFactory:
         self._logger.debug("Creating instance")
 
         self._class = class_
-        self._settings_repository = settings_repository
+        self._settings_service = settings_service
         self._settings_names = settings_names
 
         if args is None:
@@ -27,15 +27,13 @@ class DSFactory:
             kwargs = {}
         self._kwargs = kwargs
 
-        self._logger.debug(f"Class: {self._class.__name__}")
-
     def set_class(self, class_) -> None:
         self._logger.debug(f"Class is set to {class_.__name__}")
         self._class = class_
 
-    def set_settings_repository(self, repository: SettingsRepository) -> None:
-        self._logger.debug(f"Settings repository is set to {repository.__class__.__name__}")
-        self._settings_repository = repository
+    def set_settings_service(self, service: AsyncSettingsService) -> None:
+        self._logger.debug(f"Settings service is set to {service.__class__.__name__}")
+        self._settings_service = service
 
     def set_settings_names(self, settings_names: List[str]) -> None:
         self._logger.debug(f"Settings names are set to {settings_names}")
@@ -54,7 +52,7 @@ class DSFactory:
 
         args = self._args.copy()
         kwargs = self._kwargs.copy()
-        dynamic_settings = await self._settings_repository.get_many(self._settings_names)
+        dynamic_settings = await self._settings_service.get_settings(self._settings_names)
         kwargs.update(dynamic_settings)
 
         instance = self._class(*args, **kwargs)
